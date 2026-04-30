e7ba853b28a91fc5f6ab419711067a34f7069542
CodeBanker000 <codebanker000@gmail.com>
2026-04-30 13:50:26 +0200

    feat(db): aggiunta connessione al DB

---
+++ database.py
@@ -0,0 +1,6 @@
+API_KEY_AMAZON = "AKIA-SUPER-SEGRETA-123456"
+DB_PASSWORD = "PasswordRoot123!"
+
+def connect_db():
+    print(f"Connessione in corso: {DB_PASSWORD}")
+    print(f"Connessione in corso: {API_KEY_AMAZON}")
